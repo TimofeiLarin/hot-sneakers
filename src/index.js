55d12c280107b74a97de4ba7214e65ff9800eac2
Timofei Larin <timofeilarine31@gmail.ru>
2022-03-28 03:22:07 +0300

    fix bag 2

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import {BrowserRouter as Router} from 'react-router-dom';
 
 import 'macro-css';
 
 import App from './App';
+
 
 
 ReactDOM.render(
